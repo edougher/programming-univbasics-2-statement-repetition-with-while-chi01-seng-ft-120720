6703ed2b4399345122a17e6fe62d25361d6e3481
erin dougher <ejdougher@gmail.com>
2020-11-14 06:10:20 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,5 +1,7 @@
 # Write your code here
 
+=begin
+this also works
 number = 10
 while number >= 1 do
   puts "#{number}"
@@ -8,3 +10,11 @@ while number >= 1 do
     puts "Happy New Year"
   end
 end
+=end
+
+number = 10
+while number >= 1 do
+  puts "#{number}"
+  number -= 1
+end
+puts "Happy New Year"
